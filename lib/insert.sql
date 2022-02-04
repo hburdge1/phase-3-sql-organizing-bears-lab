d3ea83730614140f3bdd8f4920ee40e0ea6ff11a
hburdge1 <95104319+hburdge1@users.noreply.github.com>
2022-02-04 08:35:27 -0800

    commit tree d923196682289005e346d65ac3de928a9584d6dd

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'good', false); 
+INSERT INTO bears (name, age, sex, temperament, alive) VALUES ('Rowdy', 3, 'F', 'grumpy', true);  
+INSERT INTO bears (name, age, sex, temperament, alive) VALUES ('Tabitha', 6, 'F', 'sleepy', true);
+INSERT INTO bears (name, age, sex, temperament, alive) VALUES ('Sergeant Brown', 9, 'F', 'dopey', false);
+INSERT INTO bears (name, age, sex, temperament, alive) VALUES ('Melissa', 13, 'F', 'doc', true);
+INSERT INTO bears (name, age, sex, temperament, alive) VALUES ('Grinch', 0, 'M', 'sneezy', false);
+INSERT INTO bears (name, age, sex, temperament, alive) VALUES ('Wendy', 6, 'F', 'angry', true);
+INSERT INTO bears (name, age, sex, temperament, alive) VALUES (NULL, 1, 'F', 'cutie', false)
